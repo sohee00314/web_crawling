a677739be9048019b325993bcf19e8df24f09874
sohee00314 <sohee00314@gmail.com>
2025-09-03 23:35:01 +0900

    🎉 Start : java로 웹 크롤링 해보기

---
+++ crawler-basic/JavaWebCrawling/src/main/java/com/sinse/javawebcrawling/JavaWebCrawlingApplication.java
@@ -0,0 +1,13 @@
+package com.sinse.javawebcrawling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWebCrawlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaWebCrawlingApplication.class, args);
+    }
+
+}
