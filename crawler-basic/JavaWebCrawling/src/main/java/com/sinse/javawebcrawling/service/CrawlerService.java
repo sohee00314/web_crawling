81b1e5f9b9521ba7ac9da2615f1222b89d7678e4
sohee00314 <sohee00314@gmail.com>
2025-09-11 21:45:56 +0900

    🩹 Simple Fix : 상품의 도수, 용량, 구성, 설명 파싱 추가

--- crawler-basic/JavaWebCrawling/src/main/java/com/sinse/javawebcrawling/service/CrawlerService.java
+++ crawler-basic/JavaWebCrawling/src/main/java/com/sinse/javawebcrawling/service/CrawlerService.java
@@ -82,6 +82,10 @@ public class CrawlerService {
                         // 결과 파싱
                         p.setCategory(product.getCategory()); // 주종
                         p.setProductKind(product.getProductKind()); // 종류
+                        p.setContent(product.getContent()); //상품정보
+                        p.setAlcohol(product.getAlcohol()); //도수
+                        p.setMl(product.getMl()); // 용량
+                        p.setLineup(product.getLineup()); // 구성
                         p.setPrices(product.getPrices()); // 상품 가격리스트
                         p.setReviews(product.getReviews()); // 상품 리뷰 리스트
                         allProducts.add(p);
