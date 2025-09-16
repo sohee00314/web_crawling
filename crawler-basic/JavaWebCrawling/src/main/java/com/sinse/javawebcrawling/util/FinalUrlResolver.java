7106ba0e5f806121678356f6d277f0be9e8032b7
sohee00314 <sohee00314@gmail.com>
2025-09-16 17:23:05 +0900

    💡 Comments : 사용자 에이전트 설정

--- crawler-basic/JavaWebCrawling/src/main/java/com/sinse/javawebcrawling/util/FinalUrlResolver.java
+++ crawler-basic/JavaWebCrawling/src/main/java/com/sinse/javawebcrawling/util/FinalUrlResolver.java
@@ -1,4 +1,9 @@
 package com.sinse.javawebcrawling.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class FinalUrlResolver {
+    //본인에 맞는 사용자 에이전트 설정
+    @Value("${user-agent}")
+    private String UA;
 }
