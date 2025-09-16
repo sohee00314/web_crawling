f27443648cd3d0405b7d69894b0e79903d052546
sohee00314 <sohee00314@gmail.com>
2025-09-16 17:27:01 +0900

    🚧 WIP : 최종사이트로 링크를 추출할 메서드 구현중

--- crawler-basic/JavaWebCrawling/src/main/java/com/sinse/javawebcrawling/util/FinalUrlResolver.java
+++ crawler-basic/JavaWebCrawling/src/main/java/com/sinse/javawebcrawling/util/FinalUrlResolver.java
@@ -6,4 +6,8 @@ public class FinalUrlResolver {
     //본인에 맞는 사용자 에이전트 설정
     @Value("${user-agent}")
     private String UA;
+
+    public String resolveUrl(String url) {
+        return url;
+    }
 }
